0be172330e6a10d7ee0dfa7414de4aa50036699d
Raissa Cardoso <raissa_cardoso@yahoo.com.br>
2022-04-05 08:18:48 -0300

    alterações 05-04

--- ReactRedux/fundamentos-react/src/data/produtos.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {id:1, nome:"lápis", preco:2.90},
-    {id:2, nome:"caneta", preco:3.50},
-    {id:3, nome:"borracha", preco:1.50},
-    {id:4, nome:"caderno", preco:12.90},
-    {id:5, nome:"sulfite", preco:9.99},
-    {id:6, nome:"clipes", preco:5.80},
-]
