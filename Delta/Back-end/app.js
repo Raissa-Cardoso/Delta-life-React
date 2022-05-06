5a88434bec6e68e546865c5f356ff56b1c94b117
Raissa Cardoso <raissa_cardoso@yahoo.com.br>
2022-05-06 09:10:45 -0300

    alterações 06/05

--- Delta/Back-end/app.js
+++ Delta/Back-end/app.js
@@ -2,8 +2,7 @@ const express= require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
 
-require("./models/Artigo");
-const Artigo=mongoose.model("artigo");
+
 require("./models/Produto");
 const Produto=mongoose.model("produto");
 
@@ -27,66 +26,6 @@ mongoose.connect('mongodb://localhost/my_database',{
     console.log("Erro: Conexão com mongoDB não foi realizada")
 });
 
-/*app.get('/', (req,res)=>{    
-    Artigo.find({}).then((artigo)=>{
-        return res.json(artigo)
-    }).catch((erro)=>{
-        return res.status(400).json({
-            error:true,
-            message:"Nenhum artigo encontrado!"
-        })
-    })
-}); 
-
-app.get('/artigo/:id', (req,res)=>{        
-    Artigo.findOne({_id:req.params.id}).then((artigo)=>{
-        return res.json(artigo)
-    }).catch((erro)=>{
-        return res.status(400).json({
-            error:true,
-            message:"Nenhum artigo encontrado!"
-        })
-    })    
-}); 
-
-app.post('/artigo', (req,res)=>{
-    const artigo=Artigo.create(req.body,(err)=>{
-        if(err) return res.status(400).json({
-            error:true,
-            message:"Error: Artigo não foi cadastrado com sucesso!"
-        })
-        return res.status(400).json({
-            error:false,
-            message:"Artigo cadastrado com sucesso!"
-        })
-    })
-});
-
-app.put('/artigo/:id', (req,res)=>{        
-    const artigo=Artigo.updateOne({_id: req.params.id}, req.body, (err)=>{
-        if(err) return res.status(400).json({
-            error:true,
-            message:"Error: Artigo não foi editado com sucesso!"
-        })
-        return res.status(400).json({
-            error:false,
-            message:"Artigo editado com sucesso!"
-        })
-    }) 
-});
-
-app.delete('/artigo/:id', (req,res)=>{        
-    const artigo=Artigo.deleteOne({_id: req.params.id}, req.body, (err)=>{
-        if(err) return res.status(400).json({
-            error:true,
-            message:"Error: Artigo não foi apagado com sucesso!"
-        })
-        return res.status(400).json({
-            error:false,
-            message:"Artigo apagado com sucesso!"
-        })
-    }) 
-});*/
 
 
 
